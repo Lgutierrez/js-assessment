3a35a2b44a76b65caed840b9ffbf7bc0c55d0645
Lesley Martin del Campo <lesliz32@gmail.com>
2014-03-27 16:34:03 -0500

    all but last 3 array problems solved

--- app/arrays.js
+++ app/arrays.js
@@ -31,31 +31,43 @@ define(function() {
     },
 
     removeWithoutCopy : function(arr, item) {
-
+        var x = arr.indexOf(item);
+        for(i=arr.length-1; i>=0; i--) {
+            if(arr[i] === item) {
+                arr.splice(i, 1);
+            }
+        }
+        return arr;
     },
 
     append : function(arr, item) {
-
+        arr.push(item);
+        return arr;
     },
 
     truncate : function(arr) {
-
+        arr.pop();
+        return arr;
     },
 
     prepend : function(arr, item) {
-
+        arr.unshift(item);
+        return arr;
     },
 
     curtail : function(arr) {
-
+        arr.shift();
+        return arr;
     },
 
     concat : function(arr1, arr2) {
-
+        var result = arr1.concat(arr2);
+        return result;
     },
 
     insert : function(arr, item, index) {
-
+        arr.splice(index, 0, item);
+        return arr;
     },
 
     count : function(arr, item) {
@@ -68,11 +80,18 @@ define(function() {
     },
 
     duplicates : function(arr) {
-
+      var sorted = arr.sort();
+      var results = [];
+      for(var i=0; i < sorted.length; i++) {
+        if(sorted[i+1] == sorted[i]) {
+          results.push(sorted[i]);
+        }
+      }
+      console.log(results);
     },
 
     square : function(arr) {
-
+    
     },
 
     findAllOccurrences : function(arr, target) {
